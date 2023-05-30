5055a67d115ae798da0b8cc48388586c0a64caae
JonBoers <jon.boers@gmail.com>
2023-05-30 14:11:17 +0200

    Create PRG-03ex2.py

---
+++ 10-16_PRG/PRG-03ex2.py
@@ -0,0 +1,7 @@
+name = input("what is your name")
+age = input("how old are you")
+drink = input("What's your favourite drink?")
+qty = input("how much " + drink + " do you drink daily?")
+
+print("Hi " + name + " WOW! " + age + " is really really old! Drinking " + qty + " cups of " + drink + " a day is too much.")
+
